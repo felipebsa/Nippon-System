622beb9993b4a5f6a3313e51ca1c536363704b1a
felipessantosbsa-cypher <felipessantosbsa@gmail.com>
2026-06-27 17:02:43 -0300

    user register + user login

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -5,6 +5,9 @@ from models.user import User
 from schemas.user import SchemaLogin, SchemaRegister, SchemaUserResponse
 from database import get_db
 from passlib.context import CryptContext
+from jose import jwt
+
+pwd_context = CryptContext(schemes=['bcrypt'])
 
 router = APIRouter()
 
@@ -12,13 +15,12 @@ router = APIRouter()
 def auth_home():
     return {"message": "auth_home successful"}
 
-@router.post("auth/register")
+@router.post("/auth/register")
 def create_user(user: SchemaRegister, db: Session = Depends(get_db)):
     query = select(User).where(User.email==user.email)
     db_user = db.execute(query).scalars().first()
     if db_user:
         raise HTTPException(status_code=409, detail="email exists")
-    pwd_context = CryptContext(schemes=['bcrypt'])
     hash_created = pwd_context.hash(user.password)
     post_user = User(
         email = user.email,
@@ -28,3 +30,14 @@ def create_user(user: SchemaRegister, db: Session = Depends(get_db)):
     db.add(post_user)
     db.commit()
     return {"message": "successful create_user"}
+
+@router.post("/auth/login")
+def acess_user(user: SchemaLogin, db: Session = Depends(get_db)):
+    query = select(User).where(User.email==user.email)
+    db_user = db.execute(query).scalars().first()
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="email not exists")
+    is_valid = pwd_context.verify(user.password, db_user.pass_hash)
+    if not is_valid:
+        raise HTTPException(status=401, detail="password incorrect")
+    
