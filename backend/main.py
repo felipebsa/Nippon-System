47bebedca108f88bf654b76d43558812c2e70b96
felipessantosbsa-cypher <felipessantosbsa@gmail.com>
2026-07-11 15:16:31 -0300

    chore: add dependencys and cors origin

--- backend/main.py
+++ backend/main.py
@@ -24,7 +24,7 @@ Base.metadata.create_all(bind=engine)
 #settings Cors
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://nippon-system.netlify.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
